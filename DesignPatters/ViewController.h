e7e7b7992fb49c391b32bd227522e2c7fe4d2e86
Ashwini Tangade <ashwinitangade@macbook-pro.local>
2016-03-09 12:58:47 -0800

    Initial Commit

---
+++ DesignPatters/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesignPatters
+//
+//  Created by Ashwini Tangade on 3/9/16.
+//  Copyright © 2016 myCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
